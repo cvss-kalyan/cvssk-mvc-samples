7af62bdfbeb233cc81b03f955820ec7ee064df8b
cvss-Kalyan <cvsskalyan@gmail.com>
2024-06-08 16:59:18 +0530

    controller class

---
+++ registration-form/registration-form/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using registration_form.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace registration_form.Controllers
+{
+	public class HomeController : Controller
+	{
+		// GET: Home
+		public ActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public ActionResult submitedData(Details Det)
+		{
+			if (ModelState.IsValid)
+			{
+				return View();
+			}
+			return View("Index");
+		}
+	}
+
+}
