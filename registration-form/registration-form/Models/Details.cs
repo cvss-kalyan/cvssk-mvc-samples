08d9102d0a9b890cdf45a039a2a0c5310d54205e
cvss-Kalyan <cvsskalyan@gmail.com>
2024-06-08 17:00:50 +0530

    model and view

---
+++ registration-form/registration-form/Models/Details.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace registration_form.Models
+{
+	public class Details
+	{
+		[Required]
+		public string FirstName { get; set; }
+		[Required]
+		public string LastName { get; set; }
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+		[Required]
+		[Range(18, 100)]
+		public int Age { get; set; }
+	}
+}
